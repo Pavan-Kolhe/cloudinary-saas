f82de88a068354e2c09e635b62b704e30de79b74
Pavan-Kolhe <pavandigambarkolhe@gmail.com>
2025-08-01 23:01:33 +0530

    add middleware sign-in sign-up from clerk added app.css req for daisyUi add rest just created pages

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in",
+  "/sign-up",
+  "/",
+  "/home",
+]);
+
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth(); // id hai malab logged in hai
+  const currentUrl = new URL(req.url);
+  const isAccessingDashboard = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  //logged in
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+  //not logged in
+  if (!userId) {
+    // if user is not logged in but tries to access protected routes
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+    // if the request is for a protected API and user is not logged in
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
